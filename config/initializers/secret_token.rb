72e3af7fd35995a98e9027d80e1fa7df4dd5a784
Vijay Aura <vijayendra.iitm@gmail.com>
2013-06-22 22:54:20 +0530

    create base rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aura::Application.config.secret_token = 'f92fe6e7e6fafcb702a18eeb8b8c6ed99244d125c9a98a872af23e61b3d51b06a0179fdb59f035e771d3c35b89aa12ef9797eb0b3cc81f316eb144036d26de85'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Aura::Application.config.session_store :cookie_store, key: '_aura_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Aura::Application.config.session_store :active_record_store
