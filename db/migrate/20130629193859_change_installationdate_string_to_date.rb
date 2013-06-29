0309bfdb219bd329b9edde955a56efb5deb12968
Vijay Aura <vijayendra.iitm@gmail.com>
2013-06-30 02:01:47 +0530

    Attempt to fix the installation date type

--- db/migrate/20130629193859_change_installationdate_string_to_date.rb
+++ db/migrate/20130629193859_change_installationdate_string_to_date.rb
@@ -1,6 +1,11 @@
 class ChangeInstallationdateStringToDate < ActiveRecord::Migration
   def self.up 
   	Customer.reset_column_information
+  connection.execute(%q{
+    alter table customers
+    alter column installation_date
+    type date using cast(string as date)
+  })
   	change_column  :customers, :installation_date, :date
   end
 
